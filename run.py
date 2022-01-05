48732714907f5ef489932d315870ddd8b456f5e0
Linushf <linus.furubom@gmail.com>
2022-01-05 23:35:24 +0000

    Change double quotes to single quotes

--- run.py
+++ run.py
@@ -11,21 +11,21 @@ def generate_board():
     while valid is not True:
         try:
             board_size = int(input(
-                "How big would you like the sea? (2 - 10):\n"))
+                'How big would you like the sea? (2 - 10):\n'))
             if board_size < 2:
-                print("Too smal, enter a number between 2 - 10")
+                print('Too smal, enter a number between 2 - 10')
                 return generate_board()
             elif board_size > 10:
-                print("Too large, enter a number between 2 - 10")
+                print('Too large, enter a number between 2 - 10')
                 return generate_board()
             elif board_size <= 10 or board_size >= 2:
                 for _ in range(board_size):
-                    board.append(["O"] * board_size)
+                    board.append(['O'] * board_size)
                 valid = True
             else:
                 return generate_board()
         except ValueError:
-            print("Not a number, try again")
+            print('Not a number, try again')
             return generate_board()
         return board
 
@@ -39,30 +39,30 @@ def user_ships(board):
     valid_col = False
     user_row = None
     user_col = None
-    print("Where do you what to place your ships? ")
-    print("Enter a number between 1 - {} ".format(len(board)))
+    print('Where do you what to place your ships? ')
+    print('Enter a number between 1 - {} '.format(len(board)))
     while valid_row is not True:
         try:
-            user_row = int(input("Enter row for your ship:\n"))
+            user_row = int(input('Enter row for your ship:\n'))
             if user_row not in range(1, len(board) + 1):
-                print("Not valid row,")
-                print("enter a number 1 - {}".format(len(board)))
+                print('Not valid row,')
+                print('enter a number 1 - {}'.format(len(board)))
             else:
                 valid_row = True
         except ValueError:
-            print("Not a number, try again")
+            print('Not a number, try again')
     while valid_col is not True:
         try:
-            user_col = int(input("Enter column for your ship:\n"))
+            user_col = int(input('Enter column for your ship:\n'))
             if user_col not in range(1, len(board) + 1):
-                print("Not valid column, ")
-                print("enter a number 1 - {}".format(len(board)))
+                print('Not valid column, ')
+                print('enter a number 1 - {}'.format(len(board)))
             else:
                 valid_col = True
         except ValueError:
-            print("Not a number, try again")
+            print('Not a number, try again')
     if user_row and user_col:
-        board[user_row - 1][user_col - 1] = "#"
+        board[user_row - 1][user_col - 1] = '#'
     return board
 
 
@@ -82,7 +82,7 @@ def find_ship(board):
     while valid_enemy_location is not True:
         enemy_row = random_row_and_col(board)
         enemy_col = random_row_and_col(board)
-        if board[enemy_row][enemy_col] != "#":
+        if board[enemy_row][enemy_col] != '#':
             return enemy_row, enemy_col
 
 
@@ -91,8 +91,8 @@ def print_board(board):
     Print out the board.
     """
     for row in board:
-        print(("   ").join(row))
-    print(" ")
+        print(('   ').join(row))
+    print(' ')
 
 
 def row_guess(board):
@@ -100,17 +100,17 @@ def row_guess(board):
     Row coordinates for players shot.
     """
     valid_row = False
-    print("Enter a number between 1 - {}".format(len(board)))
+    print('Enter a number between 1 - {}'.format(len(board)))
     while valid_row is not True:
         try:
-            guess_row = int(input("Guess row: \n"))
+            guess_row = int(input('Guess row: \n'))
             if guess_row not in range(1, len(board) + 1):
-                print("Not a valid row, ")
-                print("enter a number between 1 - {}".format(len(board)))
+                print('Not a valid row, ')
+                print('enter a number between 1 - {}'.format(len(board)))
             else:
                 valid_row = True
         except ValueError:
-            print("Not a number, try again")
+            print('Not a number, try again')
     return guess_row - 1
 
 
@@ -119,17 +119,17 @@ def col_guess(board):
     Column coordinates for players shot.
     """
     valid_col = False
-    print("Enter a number between 1 - {}".format(len(board)))
+    print('Enter a number between 1 - {}'.format(len(board)))
     while valid_col is not True:
         try:
-            guess_col = int(input("Guess column: \n"))
+            guess_col = int(input('Guess column: \n'))
             if guess_col not in range(1, len(board) + 1):
-                print("Not a valid column, ")
-                print("enter a number between 1 - {}".format(len(board)))
+                print('Not a valid column, ')
+                print('enter a number between 1 - {}'.format(len(board)))
             else:
                 valid_col = True
         except ValueError:
-            print("Not a number, try again")
+            print('Not a number, try again')
     return guess_col - 1
 
 
@@ -142,22 +142,22 @@ def comp_shot(board, comp_guess_count, comp_row, comp_col):
         attack_row = random_row_and_col(board)
         attack_col = random_row_and_col(board)
 
-        if board[attack_row][attack_col] == "#":
-            print("Enemy sunk your ship and you lose\n")
-            print("Computer sank your ship ")
-            print("in {} turns".format(comp_guess_count))
+        if board[attack_row][attack_col] == '#':
+            print('Enemy sunk your ship and you lose\n')
+            print('Computer sank your ship ')
+            print('in {} turns'.format(comp_guess_count))
             valid_comp_shot = True
             return True
-        elif board[attack_row][attack_col] != "O":
+        elif board[attack_row][attack_col] != 'O':
             # Prevents computer duplicate coordinates
             continue
         elif attack_row == comp_row and attack_col == comp_col:
             # Prevents computer from shoting their own ship
             continue
         else:
-            board[attack_row][attack_col] = "*"
-            print(" - Computer fired at:")
-            print(f"Row: {attack_row + 1} Column: {attack_col + 1} MISS!\n")
+            board[attack_row][attack_col] = '*'
+            print(' - Computer fired at:')
+            print(f'Row: {attack_row + 1} Column: {attack_col + 1} MISS!\n')
             print_board(board)
             valid_comp_shot = True
     return False
@@ -172,22 +172,22 @@ def user_shot(board, user_guess_count, comp_row, comp_col):
         guess_row = row_guess(board)
         guess_col = col_guess(board)
 
-        if board[guess_row][guess_col] == "*":
-            print("Opponent already shot at that location, try again")
-        elif board[guess_row][guess_col] == "#":
-            print("Do not kill yourself...")
-        elif board[guess_row][guess_col] == "X":
-            print("You've already shot at that location, try again")
+        if board[guess_row][guess_col] == '*':
+            print('Opponent already shot at that location, try again')
+        elif board[guess_row][guess_col] == '#':
+            print('Do not kill yourself...')
+        elif board[guess_row][guess_col] == 'X':
+            print('You\'ve already shot at that location, try again')
         elif guess_row != comp_row or guess_col != comp_col:
-            board[guess_row][guess_col] = "X"
-            print(" - Users turn - ")
+            board[guess_row][guess_col] = 'X'
+            print(' - Users turn - ')
             print_board(board)
-            print(" - You fired at:")
-            print(f"Row: {guess_row + 1} Column: {guess_col + 1} MISS!\n")
+            print(' - You fired at:')
+            print(f'Row: {guess_row + 1} Column: {guess_col + 1} MISS!\n')
             valid_user_shot = True
         else:
-            print("You sunk enemy battleship!")
-            print("It took {} tries to win".format(user_guess_count))
+            print('You sunk enemy battleship!')
+            print('It took {} tries to win'.format(user_guess_count))
             return True
     return False
 
@@ -196,17 +196,17 @@ def new_game():
     """
     Ask the user if they would like to play another game.
     """
-    print("Choose YES or NO")
+    print('Choose \'YES\' or \'NO\'')
     while True:
-        answer = input("New game? \n").upper()
-        if answer == "YES":
+        answer = input('New game? \n').upper()
+        if answer == 'YES':
             main()
             return False
-        elif answer == "NO":
-            print("Thank you for playing, see you next time!")
+        elif answer == 'NO':
+            print('Thank you for playing, see you next time!')
             return False
         else:
-            print("Not a valid choice, choose 'Yes' or 'No'")
+            print('Not a valid choice, choose \'Yes\' or \'No\'')
             return new_game()
 
 
@@ -236,13 +236,13 @@ def main():
     board = generate_board()
     print_board(board)
     board = user_ships(board)
-    print("----- Time for a game of Battleship -----")
-    print("# = User ship location, X = User miss, * = Computer miss")
+    print('----- Time for a game of Battleship -----')
+    print('# = User ship location, X = User miss, * = Computer miss')
     print_board(board)
     comp_row, comp_col = find_ship(board)
     game_play(board, comp_row, comp_col)
     new_game()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
